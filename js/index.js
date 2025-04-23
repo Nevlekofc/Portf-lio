efc7c4b6dea55d208d8ad3cf7fd449630b4cf5a8
Nevlek of <kelvencuco8@gmail.com>
2025-04-23 22:43:27 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,50 @@
+   //animação na home
+     window.onload = function() {
+            const element = document.querySelector('.home-content');
+            element.classList.add('kely');
+        };
+  //animação portifolio-box
+        document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll('.portifolio-box');
+
+    function revealCards() {
+        const windowHeight = window.innerHeight;
+
+        cards.forEach(portifoliobox => {
+            const cardTop = portifoliobox.getBoundingClientRect().top;
+
+            if (cardTop < windowHeight - 100) { // Se o card estiver dentro da visão
+                portifoliobox.classList.add('visible');
+            }
+        });
+    }
+
+    // Chama a função ao rolar a página
+    window.addEventListener('scroll', revealCards);
+
+    // Chama a função uma vez ao carregar
+    revealCards();
+});
+
+//animação contato
+        document.addEventListener("DOMContentLoaded", function () {
+    const cards = document.querySelectorAll('.contato-conte');
+
+    function revealCards() {
+        const windowHeight = window.innerHeight;
+
+        cards.forEach(contatoconte => {
+            const cardTop = contatoconte.getBoundingClientRect().top;
+
+            if (cardTop < windowHeight - 100) { // Se o card estiver dentro da visão
+                contatoconte.classList.add('visible');
+            }
+        });
+    }
+
+    // Chama a função ao rolar a página
+    window.addEventListener('scroll', revealCards);
+
+    // Chama a função uma vez ao carregar
+    revealCards();
+});
